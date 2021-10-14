d9289e72fdebb2aa7f990dabb3b9f5e3bd3cf214
MrJanHorak <janny_b_goode@hotmail.com>
2021-10-14 16:54:50 -0400

    set up initial variables and constants

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*-------------------------------- Constants --------------------------------*/
+const carrier = [sw, sw, sw, sw, sw]
+const battleship = [sw, sw, sw, sw]
+const submarine = [sw, sw, sw]
+const cruiser = [sw, sw ,sw]
+const destroyer = [sw, sw]
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let numberOfHits, numOfGuesses, gameState, shipOrientation, whoWins, gamneGrid
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
