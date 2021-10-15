492c7daab65b6b4da0b666af8fdf75199bfad98c
MrJanHorak <janny_b_goode@hotmail.com>
2021-10-14 21:46:48 -0400

    add function to create html grid and add first css rules

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 /*-------------------------------- Constants --------------------------------*/
-const carrier = [sw, sw, sw, sw, sw]
-const battleship = [sw, sw, sw, sw]
-const submarine = [sw, sw, sw]
-const cruiser = [sw, sw ,sw]
-const destroyer = [sw, sw]
+// Boat arrays contain values to show thier current status
+// sw = swim   and   hi = hit
+const carrier = ["sw", "sw", "sw", "sw", "sw"]
+const battleship = ["sw", "sw", "sw", "sw"]
+const submarine = ["sw", "sw", "sw"]
+const cruiser = ["sw", "sw" ,"sw"]
+const destroyer = ["sw", "sw"]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let numberOfHits, numOfGuesses, gameState, shipOrientation, whoWins, gameGrid
+let numberOfHits, numOfGuesses, gameState, shipOrientation, whoWins, gameGridState, rows, columns
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const gameGrid = document.querySelector("#battleshipGrid")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -21,42 +23,58 @@ let numberOfHits, numOfGuesses, gameState, shipOrientation, whoWins, gameGrid
 init()
 
 function init() {
-numberOfHits = 0
-numOfGuesses = 0
-whoWins = null
-gameGrid = 'place'
-shipOrientation = 'horizontal'
 
+  rows= 10
+  columns = 10
+  numberOfHits = 0
+  numOfGuesses = 0
+  whoWins = null
+  gameGridState = 'place'
+  shipOrientation = 'horizontal'
 
-console.log('this is init()')
-render()
+  console.log('this is init()')
+  createGrid()
+  render()
 
 
 }
 
 
 
-render(){
+function render(){
   console.log('this is render()')
 }
 
-placeShip(){
+function placeShip(){
   console.log('this is place ship')
 }
 
-guessShip(){
+function guessShip(){
   console.log('This is Guess')
 }
 
-hitOrMiss(){
+function hitOrMiss(){
   console.log('this is hit or miss')
 }
 
-whoWins(){
+function winnerYet(){
   console.log('this is whoWins')
 }
 
-changeBoatStatus(){
+function changeBoatStatus(){
   console.log('this is changeBoatStatus')
 }
 
+function createGrid(){
+  console.log('this is createGrid')
+
+  gameGrid.style.setProperty('--grid-rows', rows)
+  gameGrid.style.setProperty('--grid-cols', columns)
+  for (let c = 0; c<(rows * columns);c++){
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1)
+    gameGrid.appendChild(cell).className = 'grid-item'
+    gameGrid.appendChild(cell).idName = `c`
+  }
+
+}
